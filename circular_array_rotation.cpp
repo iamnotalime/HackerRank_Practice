bb98205326fc0969edf80b1840ec234e7ad04327
iamnotalime <81209726+iamnotalime@users.noreply.github.com>
2023-01-05 20:35:46 +0700

    Add files via upload

---
+++ circular_array_rotation.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, q, k;
+    scanf("%d", &n);
+    scanf("%d", &k);
+    scanf("%d", &q);
+    int data[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &data[i]);
+    }
+    k = k%n;
+    while(q--){
+        int x;
+        scanf("%d", &x);  
+        x = x - k;
+        if(x < 0) x = x + n;  
+        printf("%d\n", data[x]);
+    }
+    return 0;
+}
+
